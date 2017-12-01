d1a1b00989ac6e632b6fb126f2a2f804d5bf5d31
Marta <martadabrowka@gmail.com>
2017-12-01 16:48:15 +0000

    added customer class

---
+++ app/src/main/java/com/example/marta/shoppingbasket/Customer.java
@@ -0,0 +1,17 @@
+package com.example.marta.shoppingbasket;
+
+/**
+ * Created by marta on 01/12/2017.
+ */
+
+public class Customer {
+    private boolean loyaltyCard;
+
+
+    public Customer(boolean loyaltyCard) {
+        this.loyaltyCard = loyaltyCard;
+    }
+
+
+
+}
---
+++ app/src/test/java/com/example/marta/shoppingbasket/CustomerTest.java
@@ -0,0 +1,8 @@
+package com.example.marta.shoppingbasket;
+
+/**
+ * Created by marta on 01/12/2017.
+ */
+
+public class CustomerTest {
+}
