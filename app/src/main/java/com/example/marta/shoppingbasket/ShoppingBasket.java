23f3dd9bce13421412451c610a86e37f57fbe55f
Marta <martadabrowka@gmail.com>
2017-12-01 14:57:56 +0000

    created a test fle for shoppingBasket

--- app/src/main/java/com/example/marta/shoppingbasket/ShoppingBasket.java
+++ app/src/main/java/com/example/marta/shoppingbasket/ShoppingBasket.java
@@ -1,8 +1,19 @@
 package com.example.marta.shoppingbasket;
 
+import java.util.ArrayList;
+
 /**
  * Created by marta on 01/12/2017.
  */
 
 public class ShoppingBasket {
+    private ArrayList<Purchasable> boughtItems;
+
+
+    public ShoppingBasket() {
+        boughtItems = new ArrayList();
+    }
+
+
+
 }
